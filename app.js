562b9ff34e56b8ee31cab8f38d07b6abe6f1585f
Swati Chandra <70850286+Swati-chandra@users.noreply.github.com>
2021-09-20 21:34:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const customer = require('./server/service/customerService');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('assets'))
+app.listen(process.env.PORT || 3000);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+app.get('/view', (req, res) => {
+    res.sendFile(__dirname + '/public/view.html');
+});
+app.get(`/tranferToList/:custId`, (req, res) => {
+    res.sendFile(__dirname + '/public/money.html');
+});
+app.get('/getAllCustomers', (req, res) => {
+    customer.getAllCustomers(req, res);
+})
+app.get('/getTransferToList/:custId', (req, res) => {
+    customer.getTransferToList(req, res);
+})
+
